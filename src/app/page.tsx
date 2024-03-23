dea1c04922837922448442d573a6c8df22993416
Po Lien Lin <polien.joe.lin@gmail.com>
2024-03-23 11:39:10 +0800

    Add tailwind as css framework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ export default function StopWatchApp() {
   return (
     <main>
       {/* Put your components here */}
-      <h2>00:00.00</h2>
+      <h2 className="">00:00.00</h2>
       <button>Lap</button>
       <button>Start</button>
     </main>
-  )
+  );
 }
