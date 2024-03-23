ac28b4afb5b68afebd75ed786e7abd9c06284795
Po Lien Lin <polien.joe.lin@gmail.com>
2024-03-23 17:27:16 +0800

    Update gitignore

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path')
+const path = require("path");
 
 const nextConfig = {
+  output: "export",
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
-}
- 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
