3e0f686477b107f53eccc6222d0301ec68d1d1c2
Po Lien Lin <polien.joe.lin@gmail.com>
2024-03-23 16:25:56 +0800

    Finish building basic stopwatch logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,92 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { formatTime } from "../util/util";
+
 export default function StopWatchApp() {
+  //* As dependency of useEffect to decide timing of recalculate elapsed time
+  const [isWatching, setIsWatching] = useState(false);
+
+  //* unit will be millisecond
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  //* To memorize id of interval
+  const intervalRef = useRef<NodeJS.Timeout | number | string | null | Timeout>(
+    null,
+  );
+
+  //* unit will be millisecond
+  const startTimeRef = useRef(0);
+
+  const watchAction = {
+    start: () => {
+      console.log("it start");
+      setIsWatching(true);
+      startTimeRef.current = Date.now() - elapsedTime;
+    },
+    reset: () => {
+      console.log("it reset");
+      setElapsedTime(0);
+      setIsWatching(false);
+    },
+    lap: () => {
+      console.log("it lap");
+    },
+    pause: () => {
+      console.log("it pause");
+      setIsWatching(false);
+    },
+  };
+
+  useEffect(() => {
+    if (isWatching) {
+      intervalRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 20);
+    }
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, [isWatching]);
+
   return (
-    <main className="w-96 mx-auto flex flex-col justify-center items-center pt-12 gap-6">
-      {/* Put your components here */}
-      <h2 className="text-lightSalmon font-medium text-7xl w-72">00:00.00</h2>
-      <div className="flex justify-between w-72">
-        <button className="text-2xl w-32 h-32 bg-rosyBrown text-white border-4 border-solid border-gray-200 rounded-full">
+    <main className="mx-auto flex w-96 flex-col items-center justify-center gap-6 pt-12">
+      <h2 className="text-7xl font-medium text-lightSalmon">
+        {formatTime(elapsedTime)}
+      </h2>
+      <div className="flex w-72 justify-between">
+        <button className="h-32 w-32 rounded-full border-4 border-solid border-gray-200 bg-rosyBrown text-2xl text-white">
           Lap
         </button>
-        <button className="text-2xl w-32 h-32 bg-goldenRod text-white border-4 border-solid border-gray-200 rounded-full">
+
+        <button
+          onClick={watchAction.start}
+          className="h-32 w-32 rounded-full border-4 border-solid border-gray-200 bg-goldenRod text-2xl text-white"
+        >
           Start
         </button>
       </div>
-      <div className="grid grid-cols-2 w-80 mt-6 gap-y-3">
-        <h3 className="text-thistle font-medium text-xl">Lap 2:</h3>
-        <h4 className="text-thistle font-medium text-xl text-end">
+      <div className="flex w-72 justify-between">
+        <button
+          onClick={watchAction.pause}
+          className="h-32 w-32 rounded-full border-4 border-solid border-gray-200 bg-red-300 text-2xl text-white"
+        >
+          pause
+        </button>
+        <button
+          onClick={watchAction.reset}
+          className="h-32 w-32 rounded-full border-4 border-solid border-gray-200 bg-gray-400 text-2xl text-white"
+        >
+          reset
+        </button>
+      </div>
+      <div className="mt-6 grid w-80 grid-cols-2 gap-y-3">
+        <h3 className="text-xl font-medium text-thistle">Lap 2:</h3>
+        <h4 className="text-end text-xl font-medium text-thistle">
           12:00:12.50
         </h4>
-        <h3 className="text-thistle font-medium text-xl">Lap 1:</h3>
-        <h4 className="text-thistle font-medium text-xl text-end">00:07.87</h4>
+        <h3 className="text-xl font-medium text-thistle">Lap 1:</h3>
+        <h4 className="text-end text-xl font-medium text-thistle">00:07.87</h4>
       </div>
     </main>
   );
