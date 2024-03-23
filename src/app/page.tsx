910740a27ae18b57fcd9053b63dc5cbf8ece8aa8
Po Lien Lin <polien.joe.lin@gmail.com>
2024-03-23 20:56:57 +0800

    Finish building logic of lapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,62 +6,65 @@ const buttonBasicStyle =
   "h-32 w-32 rounded-full border-4 border-solid border-gray-200 text-2xl font-medium";
 
 export default function StopWatchApp() {
-  //* As dependency of useEffect to decide timing of recalculate elapsed time
   const [isWatching, setIsWatching] = useState(false);
   const [haveStarted, setHaveStarted] = useState(false);
-  const [currentLap, setCurrentLap] = useState(0);
-
-  //* unit will be millisecond
-  const [elapsedTime, setElapsedTime] = useState([0, 0]);
+  const [currentLap, setCurrentLap] = useState(1);
+  const [elapsedTimes, setElapsedTimes] = useState([0, 0]);
+  const [startTimes, setStartTimes] = useState([0, 0]);
 
   //* To memorize id of interval
   const intervalRef = useRef<NodeJS.Timeout | number>(0);
-
   //* unit will be millisecond
-  const startTimeRef = useRef([0, 0]);
 
   const watchAction = {
     start: () => {
       setIsWatching(true);
       setHaveStarted(true);
-      setCurrentLap(1);
-      startTimeRef.current[0] = Date.now() - elapsedTime[0];
-      startTimeRef.current[1] = Date.now() - elapsedTime[1];
+      const newStartTimes = [...startTimes];
+      newStartTimes[0] = Date.now() - elapsedTimes[0];
+      newStartTimes[currentLap] = Date.now() - elapsedTimes[currentLap];
+      setStartTimes(newStartTimes);
     },
     reset: () => {
-      setElapsedTime([0, 0]);
-      setCurrentLap(0);
+      setElapsedTimes([0, 0]);
+      setStartTimes([0, 0]);
+      setCurrentLap(1);
       setIsWatching(false);
       setHaveStarted(false);
+      clearInterval(intervalRef.current);
     },
     lap: () => {
-      console.log("it lap");
+      const newStartTimes = [...startTimes, Date.now()];
+      const newElapsedTimes = [...elapsedTimes, 0];
       setCurrentLap((prev) => prev + 1);
+      setElapsedTimes(newElapsedTimes);
+      setStartTimes(newStartTimes);
     },
-    pause: () => {
+    stop: () => {
       setIsWatching(false);
+      clearInterval(intervalRef.current);
     },
   };
 
   useEffect(() => {
     if (isWatching) {
       intervalRef.current = setInterval(() => {
-        const newElapsedTime = [...elapsedTime];
-        newElapsedTime[0] = Date.now() - startTimeRef.current[0];
-        newElapsedTime[1] = Date.now() - startTimeRef.current[1];
-        setElapsedTime(newElapsedTime);
-      }, 20);
+        const newElapsedTimes = [...elapsedTimes];
+        newElapsedTimes[0] = Date.now() - startTimes[0];
+        newElapsedTimes[currentLap] = Date.now() - startTimes[currentLap];
+        setElapsedTimes(newElapsedTimes);
+      }, 10);
     }
 
     return () => {
       clearInterval(intervalRef.current);
     };
-  }, [isWatching, elapsedTime]);
+  }, [isWatching, elapsedTimes, currentLap, startTimes]);
 
   return (
     <main className="mx-auto flex w-96 flex-col items-center justify-center gap-6 pt-12">
       <h2 className="text-7xl font-medium text-lightSalmon">
-        {formatTime(elapsedTime[0])}
+        {formatTime(elapsedTimes[0])}
       </h2>
       <div className="flex w-72 justify-between">
         {!isWatching && haveStarted ? (
@@ -69,11 +72,12 @@ export default function StopWatchApp() {
             onClick={watchAction.reset}
             className={`${buttonBasicStyle} bg-rosyBrown text-white`}
           >
-            reset
+            Reset
           </button>
         ) : (
           <button
             disabled={!haveStarted}
+            onClick={watchAction.lap}
             className={`${buttonBasicStyle} ${haveStarted ? "bg-rosyBrown text-white" : "bg-lightBrown text-gray-600"}`}
           >
             Lap
@@ -88,23 +92,25 @@ export default function StopWatchApp() {
           </button>
         ) : (
           <button
-            onClick={watchAction.pause}
+            onClick={watchAction.stop}
             className={`${buttonBasicStyle} bg-maroon text-white`}
           >
-            pause
+            Stop
           </button>
         )}
       </div>
-      {elapsedTime[1] !== 0 && (
+      {elapsedTimes[1] !== 0 && (
         <div className="mt-6 grid w-80 grid-cols-2 gap-y-3">
-          {elapsedTime.map((lap, index) => {
-            if (index === 0) {
+          {elapsedTimes.toReversed().map((lap, reverseIndex) => {
+            const lapsAmount = elapsedTimes.length - 1;
+
+            if (reverseIndex === lapsAmount) {
               return;
             }
             return (
-              <Fragment key={index}>
+              <Fragment key={reverseIndex}>
                 <h3 className="text-xl font-medium text-thistle">
-                  Lap {index}:
+                  Lap {lapsAmount - reverseIndex}:
                 </h3>
                 <h4 className="text-end text-xl font-medium text-thistle">
                   {formatTime(lap)}
