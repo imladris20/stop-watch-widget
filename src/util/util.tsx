18ccab8020768f9f43b4d9a8d006a343fb02128d
Po Lien Lin <polien.joe.lin@gmail.com>
2024-03-24 14:57:41 +0800

    Rewrite formatTime function to more readble form

--- src/util/util.tsx
+++ src/util/util.tsx
@@ -1,24 +1,23 @@
-export const transformNumberToTwoBit = (num: number) => {
-  return String(num).padStart(2, "0");
+export const formatTimeUnit = (num: number) => {
+  return String(Math.floor(num)).padStart(2, "0");
 };
 
-export const formatTime = (millisecondsHavePassed: number) => {
-  const hours: string = transformNumberToTwoBit(
-    Math.floor(millisecondsHavePassed / (1000 * 60 * 60)),
-  );
-  const minutes = transformNumberToTwoBit(
-    Math.floor((millisecondsHavePassed / (1000 * 60)) % 60),
-  );
-  const seconds = transformNumberToTwoBit(
-    Math.floor((millisecondsHavePassed / 1000) % 60),
-  );
-  const milliseconds = transformNumberToTwoBit(
-    Math.floor((millisecondsHavePassed % 1000) / 10),
+export const formatTime = (elapsedTime: number) => {
+  const timebase = 60;
+  const accuracyOfMillisecond = 10;
+  const secondUnit = 1000;
+  const minuteUnit = secondUnit * timebase;
+  const hourUnit = minuteUnit * timebase;
+
+  const hours: string = formatTimeUnit(elapsedTime / hourUnit);
+  const minutes: string = formatTimeUnit((elapsedTime / minuteUnit) % timebase);
+  const seconds: string = formatTimeUnit((elapsedTime / secondUnit) % timebase);
+  const milliseconds: string = formatTimeUnit(
+    (elapsedTime % secondUnit) / accuracyOfMillisecond,
   );
 
   if (Number(hours) < 1) {
     return `${minutes}:${seconds}.${milliseconds}`;
   }
-
   return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 };
